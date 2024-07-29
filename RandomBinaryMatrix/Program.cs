13a97221478f1e12418c8211b5813f2a013ad3fc
mahtab mahdavi <mahtaabmahdavi@gmail.com>
2024-07-29 11:53:47 +0330

    implement sparse matrix with HashSet

---
+++ RandomBinaryMatrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SparseMatrix
+{
+    static void Main()
+    {
+        int[,] matrix = new int[100, 100];
+        Random rand = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                matrix[i, j] = rand.Next(2);
+            }
+        }
+
+        HashSet<(int, int)> onesSet = new HashSet<(int, int)>();
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                if (matrix[i, j] == 1)
+                {
+                    onesSet.Add((i, j));
+                }
+            }
+        }
+    }
+}
