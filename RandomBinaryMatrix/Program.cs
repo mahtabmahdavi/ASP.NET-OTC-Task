b2ce6d4103f22b77849eda982a807043740ff69f
mahtab mahdavi <mahtaabmahdavi@gmail.com>
2024-07-29 12:34:14 +0330

    apply clean code principles

--- RandomBinaryMatrix/Program.cs
+++ RandomBinaryMatrix/Program.cs
@@ -5,20 +5,41 @@ class SparseMatrix
 {
     static void Main()
     {
-        int[,] matrix = new int[100, 100];
-        Random rand = new Random();
-        for (int i = 0; i < 100; i++)
+        int[,] matrix = GenerateRandomMatrix(100, 100);
+        HashSet<(int, int)> onesSet = GetOnesPosition(matrix);
+
+        var closestMatrix = FindClosest3x3MatrixWithAllOnes(matrix, onesSet);
+
+        if (closestMatrix.HasValue)
         {
-            for (int j = 0; j < 100; j++)
+            Console.WriteLine($"The nearest 3x3 matrix with all ones is located at position ({closestMatrix.Value.x}, {closestMatrix.Value.y}).");
+        }
+        else
+        {
+            Console.WriteLine("No 3x3 matrix with all ones was found.");
+        }
+    }
+
+    static int[,] GenerateRandomMatrix(int rows, int cols)
+    {
+        int[,] matrix = new int[rows, cols];
+        Random rand = new Random();
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
             {
                 matrix[i, j] = rand.Next(2);
             }
         }
+        return matrix;
+    }
 
+    static HashSet<(int, int)> GetOnesPosition(int[,] matrix)
+    {
         HashSet<(int, int)> onesSet = new HashSet<(int, int)>();
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            for (int j = 0; j < 100; j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 if (matrix[i, j] == 1)
                 {
@@ -26,43 +47,40 @@ class SparseMatrix
                 }
             }
         }
-        
-        (int, int)? result = null;
+        return onesSet;
+    }
+
+    static (int x, int y)? FindClosest3x3MatrixWithAllOnes(int[,] matrix, HashSet<(int, int)> onesSet)
+    {
+        (int x, int y)? closest = null;
         int minDistance = int.MaxValue;
 
-        for (int i = 0; i <= 97; i++)
+        for (int i = 0; i <= matrix.GetLength(0) - 3; i++)
         {
-            for (int j = 0; j <= 97; j++)
+            for (int j = 0; j <= matrix.GetLength(1) - 3; j++)
             {
-                if (Is3x3MatrixAllOnes(matrix, i, j, onesSet))
+                if (Is3x3MatrixAllOnes(i, j, onesSet))
                 {
                     int distance = i + j;
                     if (distance < minDistance)
                     {
                         minDistance = distance;
-                        result = (i, j);
+                        closest = (i, j);
                     }
                 }
             }
         }
 
-        if (result.HasValue)
-        {
-            Console.WriteLine($"The nearest 3x3 matrix with all ones is located at position ({result.Value.Item1}, {result.Value.Item2}).");
-        }
-        else
-        {
-            Console.WriteLine("No 3x3 matrix with all ones was found.");
-        }
+        return closest;
     }
-    
-    static bool Is3x3MatrixAllOnes(int[,] matrix, int x, int y, HashSet<(int, int)> onesSet)
+
+    static bool Is3x3MatrixAllOnes(int startX, int startY, HashSet<(int, int)> onesSet)
     {
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                if (!onesSet.Contains((x + i, y + j)))
+                if (!onesSet.Contains((startX + i, startY + j)))
                 {
                     return false;
                 }
