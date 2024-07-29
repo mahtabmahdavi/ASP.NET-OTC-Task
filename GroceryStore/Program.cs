987fb4611755316531b97f5c6053fff702a3fc26
mahtab mahdavi <mahtaabmahdavi@gmail.com>
2024-07-29 20:01:07 +0330

    initial configuration

--- GroceryStore/Program.cs
+++ GroceryStore/Program.cs
@@ -1,70 +1,31 @@
-﻿using GroceryStore;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+builder.Services.AddControllers();
+
+// configuring Swagger/OpenAPI at
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add database (Entity Framework)
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
-    public static void Main()
-    {
-        var yesterdayEntities = new List<Entity>
-        {
-            new Entity { EntityId = Guid.NewGuid(), Name = "Apple", Price = 1.0, FetchDate = DateTime.Now.AddDays(-1) },
-            new Entity { EntityId = Guid.NewGuid(), Name = "Banana", Price = 0.5, FetchDate = DateTime.Now.AddDays(-1) },
-            new Entity { EntityId = Guid.NewGuid(), Name = "Orange", Price = 0.8, FetchDate = DateTime.Now.AddDays(-1) }
-        };
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
-        var todayEntities = new List<Entity>
-        {
-            new Entity { EntityId = yesterdayEntities[0].EntityId, Name = "Apple", Price = 1.2, FetchDate = DateTime.Now },
-            new Entity { EntityId = Guid.NewGuid(), Name = "Mango", Price = 1.5, FetchDate = DateTime.Now },
-            new Entity { EntityId = yesterdayEntities[2].EntityId, Name = "Orange", Price = 0.8, FetchDate = DateTime.Now }
-        };
-        
-        var (changedPrices,
-            removedEntities,
-            addedEntities) = ComparePrices(yesterdayEntities, todayEntities);
-        
-        Console.WriteLine("Changed Prices:");
-        PrintEntities(changedPrices);
+var app = builder.Build();
 
-        Console.WriteLine("\nRemoved Entities:");
-        PrintEntities(removedEntities);
-
-        Console.WriteLine("\nAdded Entities:");
-        PrintEntities(addedEntities);
-    }
-    
-    public static (List<Entity> changedPrices, List<Entity> removedEntities, List<Entity> addedEntities) 
-        ComparePrices(List<Entity> yesterdayEntities, List<Entity> todayEntities)
-    {
-        var yesterdayDict = yesterdayEntities.ToDictionary(e => e.EntityId, e => e);
-        var todayDict = todayEntities.ToDictionary(e => e.EntityId, e => e);
-
-        var changedPrices = new List<Entity>();
-        var removedEntities = new List<Entity>();
-        var addedEntities = new List<Entity>();
-
-        foreach (var kvp in yesterdayDict)
-        {
-            if (todayDict.TryGetValue(kvp.Key, out var todayEntity))
-            {
-                if (!kvp.Value.Price.Equals(todayEntity.Price))
-                    changedPrices.Add(todayEntity);
-            }
-            else
-                removedEntities.Add(kvp.Value);
-        }
-
-        foreach (var kvp in todayDict)
-        {
-            if (!yesterdayDict.ContainsKey(kvp.Key))
-                addedEntities.Add(kvp.Value);
-        }
-
-        return (changedPrices, removedEntities, addedEntities);
-    }
-    
-    private static void PrintEntities(List<Entity> entities)
-    {
-        foreach (var entity in entities)
-            Console.WriteLine($"ID: {entity.EntityId}, Name: {entity.Name}, Price: {entity.Price}");
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
